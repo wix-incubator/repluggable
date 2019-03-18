da68ab31b600ac0d9223146c0b6192bd9a6077f4
Itay Shtekel <shtekel1@gmail.com>
2019-03-18 15:40:56 +0200

    Change host testKit to provide Enzyme's ReactWrapper instead of react reference

--- testKit/index.tsx
+++ testKit/index.tsx
@@ -1,6 +1,6 @@
+import { mount, ReactWrapper } from 'enzyme'
 import _ from 'lodash'
 import React, { Component, ReactElement } from 'react'
-import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { AnyFeature, AnySlotKey, AppHost, AppMainView, createAppHost, FeatureHost, SlotKey } from '../index'
 import { FeatureLifecycle, PrivateFeatureHost } from '../src/api'
@@ -30,48 +30,41 @@ export function createAppHostWithPacts(features: AnyFeature[], pacts: PactApiBas
     return createAppHost([...features, pactsFeature])
 }
 
-export const renderHost = async (host: AppHost): Promise<{ root: Component | null; DOMNode: HTMLElement | null }> => {
-    const div = document.createElement('div')
-    let root = null
-    await new Promise(resolve => {
-        root = ReactDOM.render(
-            <Provider store={host.getStore()}>
-                <AppMainView host={host} />
-            </Provider>,
-            div,
-            resolve
-        ) as Component
-    })
-    return { root, DOMNode: root && (ReactDOM.findDOMNode(root) as HTMLElement) }
+export const renderHost = (host: AppHost): { root: ReactWrapper | null; DOMNode: HTMLElement | null } => {
+    const root = mount(
+        <Provider store={host.getStore()}>
+            <AppMainView host={host} />
+        </Provider>
+    ) as ReactWrapper
+    return { root, DOMNode: root && (root.getDOMNode() as HTMLElement) }
 }
 
-export const renderInHost = async (
+export const renderInHost = (
     reactElement: ReactElement<any>,
     host: AppHost = createAppHost([])
-): Promise<{
-    root: Component | null
-    parentRef: Component | null
+): {
+    root: ReactWrapper | null
+    parentWrapper: ReactWrapper | null
     DOMNode: HTMLElement | null
     host: AppHost
-}> => {
-    const div = document.createElement('div')
+} => {
     const feature = createFeatureHost(host)
-    let root = null
-    const { ref: parentRef } = await new Promise(resolve => {
-        root = ReactDOM.render(
-            <Provider store={host.getStore()}>
-                {renderFeatureComponent(feature, <div ref={ref => resolve({ ref })}>{reactElement}</div>, '')}
-            </Provider>,
-            div
-        )
-    })
 
-    const parentNode: HTMLElement = ReactDOM.findDOMNode(parentRef) as HTMLElement
+    const root = mount(
+        <Provider store={host.getStore()}>
+            {renderFeatureComponent(feature, <div data-feature-in-host="true">{reactElement}</div>, '')}
+        </Provider>
+    )
+
+    const parentWrapper = root.find('[data-feature-in-host="true"]')
 
     return {
         root,
-        DOMNode: parentRef && (_.head(parentNode.children) as HTMLElement),
-        parentRef,
+        DOMNode: parentWrapper
+            .children()
+            .first()
+            .getDOMNode() as HTMLElement,
+        parentWrapper,
         host
     }
 }
