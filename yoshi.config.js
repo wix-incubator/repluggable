bc886eae8c7e3c829a3222065cde6daf44f64639
Amir Yonatan <amiry@wix.com>
2019-04-30 17:31:39 +0300

    migrate yoshi

---
+++ yoshi.config.js
@@ -0,0 +1,25 @@
+const _ = require('lodash');
+
+const externalsMap = {
+  'react': 'React',
+  'react-dom': 'ReactDOM',
+  'lodash': '_',
+  'react-redux': 'ReactRedux',
+  'redux': 'Redux'
+};
+
+const externals = _.reduce(externalsMap, (result, val, key) => {
+  result[key] = {
+    root: val,
+    commonjs: key,
+    commonjs2: key,
+    amd: key
+  };
+  return result;
+}, {});
+
+module.exports = {
+  entry: 'index',
+  hmr: true,
+  externals
+};
