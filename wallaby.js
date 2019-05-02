b40523ee04bc0e1ac0a979e4adb4c3f9e1b51c21
Amir Yonatan <amiry@wix.com>
2019-05-02 16:39:52 +0300

    added wallaby

--- wallaby.js
+++ wallaby.js
@@ -1,1 +1,7 @@
-module.exports = require('yoshi/config/wallaby-jest');
+module.exports = (wallaby) => {
+  const config = require('yoshi/config/wallaby-jest')(wallaby);
+  config.tests.forEach(x => {
+    x.pattern = x.pattern.replace('src', 'test');
+  });
+  return config;
+};
