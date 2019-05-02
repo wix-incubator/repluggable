114ecb93162d478faa476555ed1b98b50ba6ad1e
Felix Berman <felixb@wix.com>
2019-05-02 20:28:39 +0300

    Make translations work in responsive editor

--- src/interceptEntryPoints.ts
+++ src/interceptEntryPoints.ts
@@ -1,29 +1,35 @@
 import _ from 'lodash'
-import { EntryPoint, EntryPointOrPackage, AnySlotKey } from './API'
+import { EntryPoint, EntryPointOrPackage, EntryPointInterceptor, EntryPointOrPackagesMap } from './API'
 
-export interface EntryPointInterceptor {
-    interceptName?(innerName: string): string
-    interceptGetDependencyAPIs?(innerGetDependencyAPIs: EntryPoint['getDependencyAPIs']): EntryPoint['getDependencyAPIs']
-    interceptDeclareAPIs?(innerDeclareAPIs: EntryPoint['declareAPIs']): EntryPoint['declareAPIs']
-    interceptAttach?(innerAttach: EntryPoint['attach']): EntryPoint['attach']
-    interceptDetach?(innerDetach: EntryPoint['detach']): EntryPoint['detach']
-    interceptExtend?(innerExtend: EntryPoint['extend']): EntryPoint['extend']
+// function isArray(entryPoints: EntryPointOrPackage[] | Object): entryPoints is EntryPointOrPackage[] {
+//     return (typeof (entryPoints as EntryPointOrPackage[]).length === 'number')
+// }
+
+export function interceptEntryPoints(entryPoints: EntryPointOrPackage, interceptor: EntryPointInterceptor): EntryPoint[] {
+    return (_.flatten([entryPoints]) as EntryPoint[]).map(ep => applyInterceptor(ep, interceptor))
 }
 
-export function interceptEntryPoints(entryPointsOrPackages: EntryPointOrPackage[], interceptor: EntryPointInterceptor): EntryPoint[] {
-    const entryPoints = _.flatten(entryPointsOrPackages) as EntryPoint[]
-    return entryPoints.map(ep => applyInterceptor(ep))
+export function interceptEntryPointsMap(entryPointsMap: EntryPointOrPackagesMap, interceptor: EntryPointInterceptor) {
+    const result: EntryPointOrPackagesMap = {}
 
-    function applyInterceptor(inner: EntryPoint): EntryPoint {
-        return {
-            name: interceptor.interceptName ? interceptor.interceptName(inner.name) : inner.name,
-            getDependencyAPIs: interceptor.interceptGetDependencyAPIs
-                ? interceptor.interceptGetDependencyAPIs(inner.getDependencyAPIs)
-                : inner.getDependencyAPIs,
-            declareAPIs: interceptor.interceptDeclareAPIs ? interceptor.interceptDeclareAPIs(inner.declareAPIs) : inner.declareAPIs,
-            attach: interceptor.interceptAttach ? interceptor.interceptAttach(inner.attach) : inner.attach,
-            detach: interceptor.interceptDetach ? interceptor.interceptDetach(inner.detach) : inner.detach,
-            extend: interceptor.interceptExtend ? interceptor.interceptExtend(inner.extend) : inner.extend
+    for (const key in entryPointsMap) {
+        if (entryPointsMap.hasOwnProperty(key)) {
+            result[key] = interceptEntryPoints(entryPointsMap[key], interceptor)
         }
+    }
+
+    return result
+}
+
+function applyInterceptor(inner: EntryPoint, interceptor: EntryPointInterceptor): EntryPoint {
+    return {
+        name: interceptor.interceptName ? interceptor.interceptName(inner.name) : inner.name,
+        getDependencyAPIs: interceptor.interceptGetDependencyAPIs
+            ? interceptor.interceptGetDependencyAPIs(inner.getDependencyAPIs)
+            : inner.getDependencyAPIs,
+        declareAPIs: interceptor.interceptDeclareAPIs ? interceptor.interceptDeclareAPIs(inner.declareAPIs) : inner.declareAPIs,
+        attach: interceptor.interceptAttach ? interceptor.interceptAttach(inner.attach) : inner.attach,
+        detach: interceptor.interceptDetach ? interceptor.interceptDetach(inner.detach) : inner.detach,
+        extend: interceptor.interceptExtend ? interceptor.interceptExtend(inner.extend) : inner.extend
     }
 }
