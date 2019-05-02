58012b1db7ca989a06f765b79390509ef781a778
Amir Yonatan <amiry@wix.com>
2019-05-02 17:18:20 +0300

    fixed wallaby config

--- wallaby.js
+++ wallaby.js
@@ -1,5 +1,8 @@
 module.exports = (wallaby) => {
   const config = require('yoshi/config/wallaby-jest')(wallaby);
+  config.files.push('__tests__/**/*.js');
+  config.files.push('testKit/**/*.[j|t]s');
+  config.files.push('testKit/**/*.[j|t]sx');
   config.tests.forEach(x => {
     x.pattern = x.pattern.replace('src', 'test');
   });
