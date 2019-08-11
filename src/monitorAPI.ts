c2c29be1c083c89792c5644fa2a14d1972a08210
Eyal Sadeh <eyalsa@wix.com>
2019-08-11 17:40:58 +0300

    trace improvements  (#44)
    * trace improvements #automerge
    
    * trace improvements lint
    
    * fixed lint

--- src/monitorAPI.ts
+++ src/monitorAPI.ts
@@ -1,16 +1,24 @@
 import { AppHostOptions, Shell } from './API'
 import { interceptAnyObject } from './interceptAnyObject'
+import _ from 'lodash'
 
 function mark(name: string) {
     performance && performance.mark(name)
 }
 
-function markAndMeasure(name: string, markStart: string, markEnd: string) {
-    mark(markEnd)
-    performance && performance.measure(name, markStart, markEnd)
+function markAndMeasure(trace: any[], args: any, res: any, name: string, markStart: string, markEnd: string) {
+    if (performance) {
+        mark(markEnd)
+        performance.measure(name, markStart, markEnd)
+        const measure = performance.getEntriesByName(name)
+        trace.push({ args, res, ..._.pick(measure[0], ['name', 'duration', 'startTime']) })
+        performance.clearMarks(markStart)
+        performance.clearMarks(markEnd)
+        performance.clearMeasures(name)
+    }
 }
 
-function wrapWithMeasure<TAPI>(options: AppHostOptions, func: Function, api: TAPI, args: any[], measureName: string): TAPI {
+function wrapWithMeasure<TAPI>(options: AppHostOptions, func: Function, api: TAPI, args: any[], measureName: string, trace: any[]): TAPI {
     if (options.monitoring && options.monitoring.enablePerformance) {
         const startMarkName = `${measureName} - start`
         const endMarkName = `${measureName} - end`
@@ -18,17 +26,17 @@ function wrapWithMeasure<TAPI>(options: AppHostOptions, func: Function, api: TAP
         const res = func.apply(api, args)
         if (res && res.then) {
             return res.then((apiResult: any) => {
-                markAndMeasure(measureName, startMarkName, endMarkName)
+                markAndMeasure(trace, args, res, measureName, startMarkName, endMarkName)
                 return apiResult
             })
         }
-        markAndMeasure(measureName, startMarkName, endMarkName)
+        markAndMeasure(trace, args, res, measureName, startMarkName, endMarkName)
         return res
     }
     return func.apply(api, args)
 }
 
-export function monitorAPI<TAPI>(shell: Shell, options: AppHostOptions, apiName: string, api: TAPI): TAPI {
+export function monitorAPI<TAPI>(shell: Shell, options: AppHostOptions, apiName: string, api: TAPI, trace: any[]): TAPI {
     if (options.monitoring && options.monitoring.disableMonitoring) {
         return api
     }
@@ -37,7 +45,7 @@ export function monitorAPI<TAPI>(shell: Shell, options: AppHostOptions, apiName:
         const funcId = `${apiName}::${funcName}${originalFunc.hasOwnProperty('cache') ? '(Memoized)' : ''}`
         return (...args: any[]) => {
             return shell.log.monitor(funcId, { $api: apiName, $apiFunc: funcName, $args: args }, () =>
-                wrapWithMeasure(options, originalFunc, api, args, funcId)
+                wrapWithMeasure(options, originalFunc, api, args, funcId, trace)
             )
         }
     })
