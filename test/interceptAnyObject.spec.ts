ae61864a5e3064379cb418e8fb4cc35066c3ff47
Felix Berman <felixb@wix.com>
2019-08-12 16:17:57 +0300

    Add support of nested objects to interceptAnyObject() + export in index (#46)

---
+++ test/interceptAnyObject.spec.ts
@@ -0,0 +1,166 @@
+import _ from 'lodash'
+import { interceptAnyObject, FunctionInterceptor, PropertyInterceptor } from '../src/interceptAnyObject'
+
+type LogSpy = jest.Mock<void, string[]>
+
+function takeLog(spy: LogSpy): string[] {
+    return _.flatten(spy.mock.calls)
+}
+
+function takeLogAndClear(spy: LogSpy): string[] {
+    const result = takeLog(spy)
+    spy.mockClear()
+    return result
+}
+
+interface StringDict {
+    [key: string]: string
+}
+
+function dictToString(dict: StringDict): string {
+    return Object.keys(dict)
+        .map(key => ({
+            key,
+            value: dict[key]
+        }))
+        .filter(kvp => typeof kvp.value === 'string')
+        .map(kvp => `${kvp.key}:${kvp.value}`)
+        .join(';')
+}
+
+function argToString(arg: any): string {
+    switch (typeof arg) {
+        case 'number':
+        case 'string':
+            return `${arg}`
+        case 'object':
+            return dictToString(arg)
+        case 'undefined':
+            return 'undefined'
+        default:
+            return '???'
+    }
+}
+
+interface TestTarget {
+    voidFunc(): void
+    voidFuncWithArgs(str: string, num: number, obj: StringDict): void
+    nonVoidFunc(): StringDict
+    nonVoidFuncWithArgs(str: string, num: number): StringDict
+    scalarProp: string
+    objectProp: TestTargetNested
+}
+
+interface TestTargetNested {
+    nestedFunc(): void
+    nestedProp: string
+}
+
+function createTestTarget(spy: LogSpy): TestTarget {
+    return {
+        voidFunc() {
+            spy('voidFunc')
+        },
+        voidFuncWithArgs(str: string, num: number, dict: StringDict): void {
+            spy(`voidFuncWithArgs(str=${str},num=${num},dict=${dictToString(dict)})`)
+        },
+        nonVoidFunc() {
+            spy('nonVoidFunc')
+            return {
+                aaa: '111'
+            }
+        },
+        nonVoidFuncWithArgs(str: string, num: number): StringDict {
+            spy(`nonVoidFuncWithArgs(str=${str},num=${num})`)
+            return {
+                bbb: '222'
+            }
+        },
+        scalarProp: 'original-scalar',
+        objectProp: {
+            nestedProp: 'original-nested',
+            nestedFunc() {
+                spy('objectProp.nestedFunc')
+            }
+        }
+    }
+}
+
+describe('interceptAnyObject', () => {
+    function createFuncInterceptor(spy: LogSpy): FunctionInterceptor {
+        return (name, func) => {
+            return (...args: any[]) => {
+                spy(`BEFORE:${name}(${args.map(argToString).join(';')})`)
+                const retVal = func.apply(null, args)
+                spy(`AFTER:${name}(${argToString(retVal)})`)
+                return retVal
+            }
+        }
+    }
+
+    function createPropInterceptor(spy: LogSpy): PropertyInterceptor {
+        return (name, value) => {
+            return typeof value === 'string' ? value.replace('original', `INTERCEPTED[${name}]`) : value
+        }
+    }
+
+    it('should return same members if no interceptors passed', () => {
+        const spy: LogSpy = jest.fn()
+        const real = createTestTarget(spy)
+        const intercepted = interceptAnyObject(real)
+
+        expect(intercepted.voidFunc).toBe(real.voidFunc)
+        expect(intercepted.scalarProp).toBe(real.scalarProp)
+        expect(intercepted.objectProp.nestedFunc).toBe(real.objectProp.nestedFunc)
+    })
+
+    it('should intercept void functions', () => {
+        const spy: LogSpy = jest.fn()
+        const real = createTestTarget(spy)
+        const intercepted = interceptAnyObject(real, createFuncInterceptor(spy))
+
+        intercepted.voidFunc()
+
+        const log = takeLog(spy)
+        expect(log).toEqual(['BEFORE:voidFunc()', 'voidFunc', 'AFTER:voidFunc(undefined)'])
+    })
+
+    it('should intercept non-void functions with arguments', () => {
+        const spy: LogSpy = jest.fn()
+        const real = createTestTarget(spy)
+        const intercepted = interceptAnyObject(real, createFuncInterceptor(spy))
+
+        const retVal = intercepted.nonVoidFuncWithArgs('abc', 123)
+
+        const log = takeLog(spy)
+        expect(log).toEqual([
+            'BEFORE:nonVoidFuncWithArgs(abc;123)',
+            'nonVoidFuncWithArgs(str=abc,num=123)',
+            'AFTER:nonVoidFuncWithArgs(bbb:222)'
+        ])
+        expect(dictToString(retVal)).toBe('bbb:222')
+    })
+
+    it('should intercept scalar props', () => {
+        const spy: LogSpy = jest.fn()
+        const real = createTestTarget(spy)
+        const intercepted = interceptAnyObject(real, createFuncInterceptor(spy), createPropInterceptor(spy))
+
+        const propValue = intercepted.scalarProp
+
+        expect(propValue).toBe('INTERCEPTED[scalarProp]-scalar')
+    })
+
+    it('should intercept nested objects', () => {
+        const spy: LogSpy = jest.fn()
+        const real = createTestTarget(spy)
+        const intercepted = interceptAnyObject(real, createFuncInterceptor(spy), createPropInterceptor(spy))
+
+        const nestedPropValue = intercepted.objectProp.nestedProp
+        const nestedFuncRetVal = intercepted.objectProp.nestedFunc()
+
+        const log = takeLog(spy)
+        expect(log).toEqual(['BEFORE:objectProp.nestedFunc()', 'objectProp.nestedFunc', 'AFTER:objectProp.nestedFunc(undefined)'])
+        expect(nestedPropValue).toBe('INTERCEPTED[objectProp.nestedProp]-nested')
+    })
+})
