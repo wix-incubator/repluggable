035724310ae1c8080d9cfda7ad11fe6036927472
Felix Berman <felixb@wix.com>
2019-08-12 14:58:19 +0300

    Add support of nested objects to interceptAnyObject() + export in index

--- src/interceptAnyObject.ts
+++ src/interceptAnyObject.ts
@@ -11,6 +11,13 @@ export function interceptAnyObject<T extends AnyObject>(inner: T, onFunction?: F
         if (_.isFunction(original) && _.isFunction(onFunction)) {
             return onFunction(key, original)
         }
+        if (_.isObjectLike(original)) {
+            return interceptAnyObject(
+                original,
+                onFunction ? (name, func) => onFunction(`${key}.${name}`, func) : undefined,
+                onProperty ? (name, value) => onProperty(`${key}.${name}`, value) : undefined
+            )
+        }
         if (_.isFunction(onProperty)) {
             return onProperty(key, original)
         }
