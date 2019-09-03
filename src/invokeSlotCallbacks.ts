198352f6c609177c8d2741f63e56e448429dbd35
Haim Houri <haimh@wix.com>
2019-09-03 15:45:46 +0300

    Added invokeSlotCallbacks functinality

---
+++ src/invokeSlotCallbacks.ts
@@ -0,0 +1,19 @@
+import { ExtensionSlot } from './API'
+
+export function invokeSlotCallbacks<T extends any[]>(slot: ExtensionSlot<(...args: T) => void | Promise<void>>, ...args: T): any {
+    const slotItems = slot.getItems()
+
+    if (slot.host.options.monitoring.disableMonitoring) {
+        slotItems.forEach(slotItem => {
+            try {
+                slotItem.contribution(...args)
+            } catch (e) {
+                console.error(e)
+            }
+        })
+    }
+    slotItems.forEach(slotItem => {
+        const messageId = `${slot.host}-${slot.name}:${slotItem.shell.name}${slotItem.name && '-' + slotItem.name}`
+        slotItem.shell.log.monitor(messageId, {}, () => slotItem.contribution(...args))
+    })
+}
