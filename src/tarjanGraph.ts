1c777c9fb50c1a12d1232a14a1c6ee67ad0e8f4f
Amir Yonatan <amiry@wix.com>
2020-02-05 14:17:52 +0200

    implement using tarjan

--- src/tarjanGraph.ts
+++ src/tarjanGraph.ts
@@ -1,19 +1,18 @@
 // Based on https://gist.github.com/chadhutchins/1440602
 
 export class Graph {
-    map: Map<string, Vertex>
-    constructor() {
-        this.map = new Map<string, Vertex>()
+    private readonly map = new Map<string, Vertex>()
+    private addOrGetVertex(value: string) {
+        let vertex = this.map.get(value)
+        if (!vertex) {
+            vertex = new Vertex(value)
+            this.map.set(value, vertex)
+        }
+        return vertex
     }
 
-    private addOrGetVertex(value: string) {
-        if (!this.map.has(value)) {
-            const vertex = new Vertex(value)
-            this.map.set(value, vertex)
-            return vertex
-        }
-
-        return this.map.get(value) as Vertex
+    getVertices() {
+        return this.map.values()
     }
 
     addConnection(source: string, target: string) {
@@ -22,18 +21,14 @@ export class Graph {
 }
 
 class Vertex {
-    connections: Vertex[]
-    index: number
-    lowLink: number
-    constructor(public name: string) {
-        this.connections = []
-        this.index = -1
-        this.lowLink = -1
-    }
+    connections: Vertex[] = []
+    index = -1
+    lowLink = -1
+    constructor(public name: string) {}
 }
 
 class VertexStack {
-    constructor(public vertices: Vertex[]) {}
+    public vertices: Vertex[] = []
     contains(vertex: Vertex) {
         for (const v of this.vertices) {
             if (v.name === vertex.name) {
@@ -45,17 +40,13 @@ class VertexStack {
 }
 
 export class Tarjan {
-    index: number
-    stack: VertexStack
-    scc: Vertex[][]
-    constructor(private readonly graph: Graph) {
-        this.index = 0
-        this.stack = new VertexStack([])
-        this.scc = []
-    }
+    private index = 0
+    private readonly stack = new VertexStack()
+    private readonly scc: Vertex[][] = []
+    constructor(private readonly graph: Graph) {}
 
     run() {
-        for (const v of this.graph.map.values()) {
+        for (const v of this.graph.getVertices()) {
             if (v.index < 0) {
                 this.strongConnect(v)
             }
