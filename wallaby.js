4539f5c6991b662d6ee3e3337f2b56fe8d13b112
Amir Yonatan <amiry@wix.com>
2020-05-19 10:43:28 +0300

    remove usage of jest

--- wallaby.js
+++ wallaby.js
@@ -1,10 +1,3 @@
-module.exports = wallaby => {
-  const config = require('yoshi/config/wallaby-jest')(wallaby);
-  config.files.push('__tests__/**/*.js');
-  config.files.push('testKit/**/*.[j|t]s');
-  config.files.push('testKit/**/*.[j|t]sx');
-  config.tests.forEach(x => {
-    x.pattern = x.pattern.replace('src', 'test');
-  });
-  return config;
-};
+module.exports = () => ({
+  autoDetect: true,
+});
