1817890593f0c39e5f4d7c3d272228121645afa5
Amir Yonatan <amiry@wix.com>
2020-05-21 14:45:37 +0300

    added more changes towards bundle

--- bundle.js
+++ bundle.js
@@ -1,14 +1,58 @@
-const task = require('haste-task-webpack');
+process.env.NODE_ENV = 'production';
+const webpack = require('webpack');
+const {
+  createBaseWebpackConfig,
+} = require('yoshi-common/build/webpack.config');
 
-const developmentTask = task({
-  configPath: require.resolve('yoshi-flow-legacy/config/webpack.config.client.js'),
-  callbackPath: require.resolve('yoshi-flow-legacy/src/webpack-debug-callback.js'),
-  configParams: { isDebug: true }
-});
+function createClientWebpackConfig({
+  isDebug = true,
+} = {}) {
+  const webpackConfig = createBaseWebpackConfig({
+    target: 'web',
+    isDev: isDebug,
+    isHot: false,
+    isAnalyze: false,
+    includeStyleLoaders: false,
+    forceEmitSourceMaps: undefined,
+    useYoshiServer: false,
+    useProgressBar: false,
+    name: '@wix/repluggable',
+    useTypeScript: true,
+    useAngular: false,
+    devServerUrl: 'http://localhost:3201/',
+    separateCss: true,
+    keepFunctionNames: false,
+    separateStylableCss: false,
+    experimentalRtlCss: false,
+    cssModules: true,
+    externalizeRelativeLodash: false,
+    performanceBudget: false,
+    enhancedTpaStyle: false,
+    tpaStyle: false,
+  });
 
-// const productionTask = task({
-//   configPath: require.resolve('yoshi-flow-legacy/config/webpack.config.client.js'),
-//   configParams: { isDebug: false }
-// });
+  webpackConfig.entry = { repluggable: 'index' };
+  webpackConfig.resolve.alias = {};
+  webpackConfig.externals = {
+    react: 'React',
+    'react-dom': 'ReactDOM',
+    lodash: '_',
+    'react-redux': 'ReactRedux',
+    redux: 'Redux'
+  };
 
-Promise.all([developmentTask]);
+  webpackConfig.output = {
+    ...webpackConfig.output,
+    umdNamedDefine: true,
+    library: 'repluggable',
+    libraryTarget: 'umd',
+    globalObject: "(typeof self !== 'undefined' ? self : this)",
+  };
+
+  return webpackConfig;
+}
+
+const configDev = createClientWebpackConfig({ isDebug: true });
+const configProd = createClientWebpackConfig({ isDebug: false });
+webpack(configDev).run();
+webpack(configProd).run();
