d74d3b42e1860c66c52703fdd5a2c64defe0533c
Amir Yonatan <amiry@wix.com>
2020-05-21 13:37:01 +0300

    added more changes towards bundle

--- bundle.js
+++ bundle.js
@@ -2,12 +2,13 @@ const task = require('haste-task-webpack');
 
 const developmentTask = task({
   configPath: require.resolve('yoshi-flow-legacy/config/webpack.config.client.js'),
+  callbackPath: require.resolve('yoshi-flow-legacy/src/webpack-debug-callback.js'),
   configParams: { isDebug: true }
 });
 
-const productionTask = task({
-  configPath: require.resolve('yoshi-flow-legacy/config/webpack.config.client.js'),
-  configParams: { isDebug: false }
-});
+// const productionTask = task({
+//   configPath: require.resolve('yoshi-flow-legacy/config/webpack.config.client.js'),
+//   configParams: { isDebug: false }
+// });
 
-Promise.all([developmentTask, productionTask]);
+Promise.all([developmentTask]);
