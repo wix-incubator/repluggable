9fe1ed2be935b63954b81d7b9e8f1ca530fb71f0
Amir Yonatan <amiry@wix.com>
2020-08-02 16:59:39 +0300

    disasble hmr logs

--- src/hot.ts
+++ src/hot.ts
@@ -25,33 +25,27 @@ export const hot = (sourceModule: any, entryPoints: EntryPoint[]): EntryPoint[] 
         return entryPoints // not a dev environment
     }
 
-    const shortModuleId = sourceModule.id.split('/').pop()
-    console.log(
-        `----- HMR[${shortModuleId}] > ENTER`,
-        entryPoints.map(ep => ep.name)
-    )
+    // const shortModuleId = sourceModule.id.split('/').pop()
+    // console.log(`----- HMR[${shortModuleId}] > ENTER`, entryPoints.map(ep => ep.name))
     getOrAddModuleEntry(sourceModule.id, entryPoints).newEntryPoints = entryPoints
 
     if (sourceModule.hot) {
         sourceModule.hot.accept()
 
         if (sourceModule.hot.addStatusHandler && sourceModule.hot.status() === 'idle') {
-            console.log(`---- HMR[${shortModuleId}] > ADD STATUS HANDLER`)
+            // console.log(`---- HMR[${shortModuleId}] > ADD STATUS HANDLER`)
 
             sourceModule.hot.addStatusHandler(async (status: string) => {
-                console.log(`-------- HMR[${shortModuleId}] > statusHandler(${status})`)
+                // console.log(`-------- HMR[${shortModuleId}] > statusHandler(${status})`)
                 if (status === 'apply') {
                     setTimeout(async () => {
                         const entry = getOrAddModuleEntry(sourceModule.id, entryPoints)
                         if (entry.newEntryPoints) {
-                            console.log(`----- HMR[${shortModuleId}] > REMOVING SHELLS >`, entry.oldShellNames)
+                            // console.log(`----- HMR[${shortModuleId}] > REMOVING SHELLS >`, entry.oldShellNames)
 
                             await window.repluggableAppDebug.host.removeShells(entry.oldShellNames)
 
-                            console.log(
-                                `----- HMR[${shortModuleId}] > ADDING SHELLS >`,
-                                entry.newEntryPoints.map(ep => ep.name)
-                            )
+                            // console.log(`----- HMR[${shortModuleId}] > ADDING SHELLS >`, entry.newEntryPoints.map(ep => ep.name))
 
                             window.repluggableAppDebug.host.addShells(entry.newEntryPoints)
                             entry.oldShellNames = entry.newEntryPoints.map(ep => ep.name)
