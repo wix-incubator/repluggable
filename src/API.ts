415f05a1356e49c8ee69c34eabc79d4633e449df
Itay Shtekel <shtekel1@gmail.com>
2020-09-02 16:59:02 +0300

    Add initial API reference annotations

--- src/API.ts
+++ src/API.ts
@@ -23,20 +23,75 @@ export interface AnySlotKey {
     readonly public?: boolean // TODO: Move to new interface - APIKey
 }
 
+/**
+ * A key that represents an {ExtensionSlot} of shape T that's held in the {AppHost}
+ * Created be calling {Shell.declareSlot}
+ * Retrieved by calling {Shell.getSlot} (scoped to specific {Shell})
+ *
+ * @export
+ * @interface SlotKey
+ * @extends {AnySlotKey}
+ * @template T
+ */
 export interface SlotKey<T> extends AnySlotKey {
-    readonly empty?: T // holds no value, only triggers type-checking of T
+    /**
+     * Holds no value, only triggers type-checking of T
+     */
+    readonly empty?: T
+    /**
+     * Application layer that will restrict usage of APIs contributed by this entry point.
+     * Layers hierarchy is defined in the host options
+     * @See {AppHostOptions.layers}
+     */
     readonly layer?: string // TODO: Move to new interface - APIKey
+    /**
+     * Version of the API that will be part of the API key unique identification
+     */
     readonly version?: number // TODO: Move to new interface - APIKey
 }
 
+/**
+ * Application part that will receive a {Shell} when loaded into the {AppHost}
+ * @export
+ * @interface EntryPoint
+ */
 export interface EntryPoint {
+    /**
+     * Unique name that will represent this entry point in the host
+     */
     readonly name: string
     readonly tags?: EntryPointTags
+    /**
+     * Application layer that will restrict usage of APIs contributed by this entry point.
+     * Layers hierarchy is defined in the host options
+     * See {AppHostOptions.layers}
+     */
     readonly layer?: string
+    /**
+     * Define which API keys (a.k.a. contracts) are mandatory for this entry point to be executed
+     * @return {SlotKey<any>[]} API keys to wait for implementation
+     */
     getDependencyAPIs?(): SlotKey<any>[]
+    /**
+     * Define which API keys (a.k.a. contracts) this entry point is going to implement and contribute
+     * @return {SlotKey<any>[]} API keys that will be contributed
+     */
     declareAPIs?(): SlotKey<any>[]
+    /**
+     * Execute logic that is independent from other entry points
+     * Most commonly - contribute APIs and state
+     * @param {Shell} shell
+     */
     attach?(shell: Shell): void
+    /**
+     * Execute logic that is dependent on other entry points
+     * @param {Shell} shell
+     */
     extend?(shell: Shell): void
+    /**
+     * Clean side effects
+     * @param {Shell} shell
+     */
     detach?(shell: Shell): void
 }
 
@@ -47,14 +102,57 @@ export interface EntryPointOrPackagesMap {
 }
 
 export type ExtensionItemFilter<T> = (extensionItem: ExtensionItem<T>) => boolean
+/**
+ * A slot/container for holding any contribution of shape T
+ * Access to the slot is scoped to the {Shell}
+ *
+ * @export
+ * @interface ExtensionSlot
+ * @template T
+ */
 export interface ExtensionSlot<T> {
+    /**
+     * a unique identifier for the slot
+     */
     readonly name: string
     readonly host: AppHost
+    /**
+     * Which {Shell} owns this slot
+     */
     readonly declaringShell?: Shell
+    /**
+     * Add an item to the slot
+     *
+     * @param {Shell} shell Who owns the contributed item
+     * @param {T} item Extension item to be added to the slot
+     * @param {ContributionPredicate} [condition] A predicate to condition the retrieval of the item when slot items are requested with {ExtensionSlot<T>.getItems}
+     */
     contribute(shell: Shell, item: T, condition?: ContributionPredicate): void
+    /**
+     * Get all items contributed to the slot
+     *
+     * @param {boolean} [forceAll] Ignore items' contribution predicates and get all anyway
+     * @return {ExtensionItem<T>[]} All items contributed to the slot
+     */
     getItems(forceAll?: boolean): ExtensionItem<T>[]
+    /**
+     * Get the first item in the slot
+     *
+     * @return {ExtensionItem<T>} The first item in the slot
+     */
     getSingleItem(): ExtensionItem<T>
+    /**
+     * Get a specific item in the slot
+     *
+     * @param {string} name Extension item name
+     * @return {ExtensionItem<T>} Extension item
+     */
     getItemByName(name: string): ExtensionItem<T>
+    /**
+     * Remove items from the slot by predicate
+     *
+     * @param {ExtensionItemFilter<T>} predicate Remove all items matching this predicate
+     */
     discardBy(predicate: ExtensionItemFilter<T>): void
 }
 
@@ -69,10 +167,26 @@ export interface CustomExtensionSlot<T> extends CustomExtensionSlotHandler<T> {
     readonly declaringShell?: Shell
 }
 
+/**
+ * Item of shape T that is contributed to a slot of shape T
+ *
+ * @export
+ * @interface ExtensionItem
+ * @template T
+ */
 export interface ExtensionItem<T> {
     readonly name?: string
+    /**
+     * Which {Shell} owns this item
+     */
     readonly shell: Shell
+    /**
+     * Contribution content
+     */
     readonly contribution: T
+    /**
+     * Condition for the retrieval of this item by {ExtensionSlot<T>.getItems}
+     */
     readonly condition: ContributionPredicate
     readonly uniqueId: string
 }
@@ -81,15 +195,69 @@ export interface ExtensionItem<T> {
 // addPackages(packages: EntryPointOrPackage[])
 //
 
+/**
+ * An application content container that will accept {EntryPoint} and provide registry for contracts
+ *
+ * @export
+ * @interface AppHost
+ */
 export interface AppHost {
+    /**
+     * Get the root store of the application
+     *
+     * @return {ThrottledStore}
+     */
     getStore(): ThrottledStore
+    /**
+     * Get an implementation of API previously contributed to the {AppHost}
+     *
+     * @template TAPI
+     * @param {SlotKey<TAPI>} key API Key
+     * @return {*}  {TAPI}
+     */
     getAPI<TAPI>(key: SlotKey<TAPI>): TAPI
+    /**
+     * Get an extension slot defined on the host
+     *
+     * @template TItem
+     * @param {SlotKey<TItem>} key
+     * @return {ExtensionSlot<TItem>}
+     */
     getSlot<TItem>(key: SlotKey<TItem>): ExtensionSlot<TItem>
+    /**
+     * Get all the extension slots defined on the host
+     *
+     * @return {*}  {AnySlotKey[]}
+     */
     getAllSlotKeys(): AnySlotKey[]
+    /**
+     * Get all {EntryPoint}s addded to the {AppHost}
+     *
+     * @return {EntryPointsInfo[]}
+     */
     getAllEntryPoints(): EntryPointsInfo[]
+    /**
+     * Does the {AppHost} contain a specific {Shell}
+     *
+     * @param {string} name
+     * @return {boolean}
+     */
     hasShell(name: string): boolean
+    // TODO: Deprecate
     isLazyEntryPoint(name: string): boolean
+    /**
+     * Dynamically add {Shell}s after the host is created
+     *
+     * @param {EntryPointOrPackage[]} entryPointsOrPackages New packages or entry points to be added to the {AppHost}
+     * @return {Promise<void>}
+     */
     addShells(entryPointsOrPackages: EntryPointOrPackage[]): Promise<void>
+    /**
+     * Dynamically remove {Shell}s after the host is created
+     *
+     * @param {string[]} names {Shell} names to be removed
+     * @return {Promise<void>}
+     */
     removeShells(names: string[]): Promise<void>
     onShellsChanged(callback: ShellsChangedCallback): string
     removeShellsChangedCallback(callbackId: string): void
@@ -147,31 +315,117 @@ export interface ContributeAPIOptions<TAPI> {
 
 export type AnyFunction = (...args: any[]) => any
 export type FunctionWithSameArgs<F extends AnyFunction> = (...args: Parameters<F>) => any
+/**
+ * An scoped communication terminal provided for an {EntryPoint}
+ * in order to contribute its application content to the {AppHost}
+ *
+ * @export
+ * @interface Shell
+ * @extends {(Pick<AppHost, Exclude<keyof AppHost, 'getStore' | 'log' | 'options'>>)}
+ */
 export interface Shell extends Pick<AppHost, Exclude<keyof AppHost, 'getStore' | 'log' | 'options'>> {
+    /**
+     * Unique name of the matching {EntryPoint}
+     */
     readonly name: string
     readonly log: ShellLogger
+    /**
+     * Get store that is scoped for this {Shell}
+     *
+     * @template TState
+     * @return {ScopedStore<TState>} Scoped store for this {Shell}
+     */
     getStore<TState>(): ScopedStore<TState>
+    /**
+     * Are APIs ready to be requested
+     *
+     * @return {*}  {boolean}
+     */
     canUseAPIs(): boolean
+    /**
+     * Is store ready to be requested
+     *
+     * @return {*}  {boolean}
+     */
     canUseStore(): boolean
+    /**
+     * Did the execution of {EntryPoint}s' lifecycle phases (attach, detach) are done
+     *
+     * @return {*}  {boolean}
+     */
     wasInitializationCompleted(): boolean
     runLateInitializer<T>(initializer: () => T): T
+    /**
+     * Create an {ExtensionSlot}
+     *
+     * @template TItem
+     * @param {SlotKey<TItem>} key Key that will represent the slot (an will be used for retrieval)
+     * @return {ExtensionSlot<TItem>} Actual slot
+     */
     declareSlot<TItem>(key: SlotKey<TItem>): ExtensionSlot<TItem>
     declareCustomSlot<TItem>(key: SlotKey<TItem>, handler: CustomExtensionSlotHandler<TItem>): CustomExtensionSlot<TItem>
     // TODO: Fix contributeAPI factory type not to resort to lowest common
+    /**
+     * Contribute an implementation of an API (a.k.a contract)
+     *
+     * @template TAPI
+     * @param {SlotKey<TAPI>} key API Key that represents an interface TAPI
+     * @param {() => TAPI} factory Create an implementation of TAPI
+     * @param {ContributeAPIOptions<TAPI>} [options] Contribution options {ContributeAPIOptions}
+     * @return {TAPI} Result of the factory execution
+     */
     contributeAPI<TAPI>(key: SlotKey<TAPI>, factory: () => TAPI, options?: ContributeAPIOptions<TAPI>): TAPI
+    /**
+     * Contribute a Redux reducer that will be added to the host store
+     *
+     * @template TState
+     * @param {ReducersMapObjectContributor<TState>} contributor
+     */
     contributeState<TState>(contributor: ReducersMapObjectContributor<TState>): void
+    /**
+     * Contribute the main view (root) of the application
+     * Intended to be used by a single {Shell} in an application
+     *
+     * @param {Shell} fromShell Who owns the main view
+     * @param {ReactComponentContributor} contributor Create the main view component
+     */
     contributeMainView(fromShell: Shell, contributor: ReactComponentContributor): void
     contributeBoundaryAspect(component: ShellBoundaryAspect): void
+    /**
+     * Create a function with internal cache until any state change in the {AppHost} store
+     *
+     * @template T
+     * @param {T} func Function to build cache for
+     * @param {FunctionWithSameArgs<T>} resolver Key creator to index results by
+     * @param {() => boolean} [shouldClear] Custom clear condition (if not provided, behaves like () => true)
+     * @return {*}  {(((...args: Parameters<T>) => ReturnType<T>) & Partial<_.MemoizedFunction> & Partial<MemoizeMissHit>)}
+     */
     memoizeForState<T extends AnyFunction>(
         func: T,
         resolver: FunctionWithSameArgs<T>,
         shouldClear?: () => boolean
     ): ((...args: Parameters<T>) => ReturnType<T>) & Partial<_.MemoizedFunction> & Partial<MemoizeMissHit>
+    /**
+     * Manually trigger clear condition for function memoized with {Shell.memoizeForState}
+     */
     flushMemoizedForState(): void
+    /**
+     * Create a function with internal cache
+     *
+     * @template T
+     * @param {T} func Function to build cache for
+     * @param {FunctionWithSameArgs<T>} resolver Key creator to index results by
+     * @return {*}  {(((...args: Parameters<T>) => ReturnType<T>) & Partial<_.MemoizedFunction> & Partial<MemoizeMissHit>)}
+     */
     memoize<T extends AnyFunction>(
         func: T,
         resolver: FunctionWithSameArgs<T>
     ): ((...args: Parameters<T>) => ReturnType<T>) & Partial<_.MemoizedFunction> & Partial<MemoizeMissHit>
+    /**
+     * Clear cache of a memoized function
+     *
+     * @param {(Partial<_.MemoizedFunction> & Partial<MemoizeMissHit>)} memoizedFunction
+     */
     clearCache(memoizedFunction: Partial<_.MemoizedFunction> & Partial<MemoizeMissHit>): void
 }
 
