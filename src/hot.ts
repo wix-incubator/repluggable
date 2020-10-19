7eb2c0a63beea491ac8aed86344f77efb663840a
Amir Yonatan <amiry@wix.com>
2020-10-19 18:11:57 +0300

    making hmr great again

--- src/hot.ts
+++ src/hot.ts
@@ -1,25 +1,23 @@
 import { EntryPoint } from './API'
 
 export const hot = (sourceModule: any, entryPoints: EntryPoint[]): EntryPoint[] => {
-    if (!sourceModule.hot) {
-        return entryPoints // not a dev environment
-    }
+    if (sourceModule.hot) {
+        const shortModuleId = sourceModule.id.split('/').pop()
 
-    const shortModuleId = sourceModule.id.split('/').pop()
+        sourceModule.hot.accept()
+        sourceModule.hot.dispose(() => {
+            const oldShellNames = entryPoints.map(x => x.name)
+            console.debug(`----- HMR[${shortModuleId}] > REMOVING SHELLS >`, oldShellNames)
+            return window.repluggableAppDebug.host.removeShells(oldShellNames)
+        })
 
-    sourceModule.hot.accept()
-    sourceModule.hot.dispose(() => {
-        const oldShellNames = entryPoints.map(x => x.name)
-        console.debug(`----- HMR[${shortModuleId}] > REMOVING SHELLS >`, oldShellNames)
-        return window.repluggableAppDebug.host.removeShells(oldShellNames)
-    })
-
-    if (sourceModule.hot.status() === 'apply') {
-        console.debug(
-            `----- HMR[${shortModuleId}] > ADDING SHELLS >`,
-            entryPoints.map(x => x.name)
-        )
-        window.repluggableAppDebug.host.addShells(entryPoints)
+        if (sourceModule.hot.status() === 'apply') {
+            console.debug(
+                `----- HMR[${shortModuleId}] > ADDING SHELLS >`,
+                entryPoints.map(x => x.name)
+            )
+            window.repluggableAppDebug.host.addShells(entryPoints)
+        }
     }
 
     return entryPoints
