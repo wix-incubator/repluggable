064cc4f737b80bd52884e4ca354193a679cd2ca0
Amir Yonatan <amiry@wix.com>
2020-10-20 10:24:43 +0300

    making hmr great again (#131)
    * making hmr great again
    
    * making hmr great again
    
    * making hmr great again

--- src/hot.ts
+++ src/hot.ts
@@ -1,28 +1,8 @@
 import { EntryPoint } from './API'
 
-interface ModuleEntry {
-    oldShellNames: string[]
-    newEntryPoints?: EntryPoint[]
-}
-
-const moduleById: Record<string, ModuleEntry> = {}
-
-function getOrAddModuleEntry(moduleId: string, lastKnownEntryPoints: EntryPoint[]): ModuleEntry {
-    const existingEntry = moduleById[moduleId]
-    if (existingEntry) {
-        return existingEntry
-    }
-    const newEntry: ModuleEntry = {
-        oldShellNames: lastKnownEntryPoints.map(ep => ep.name),
-        newEntryPoints: undefined
-    }
-    moduleById[moduleId] = newEntry
-    return newEntry
-}
-
 export const hot = (sourceModule: any, entryPoints: EntryPoint[]): EntryPoint[] => {
     if (!sourceModule.hot) {
-        return entryPoints // not a dev environment
+        return entryPoints
     }
 
     const urlParams = new URLSearchParams(window.location.search)
@@ -31,41 +11,20 @@ export const hot = (sourceModule: any, entryPoints: EntryPoint[]): EntryPoint[] 
     }
 
     const shortModuleId = sourceModule.id.split('/').pop()
-    console.debug(
-        `----- HMR[${shortModuleId}] > ENTER`,
-        entryPoints.map(ep => ep.name)
-    )
-    getOrAddModuleEntry(sourceModule.id, entryPoints).newEntryPoints = entryPoints
 
-    if (sourceModule.hot) {
-        sourceModule.hot.accept()
+    sourceModule.hot.accept()
+    sourceModule.hot.dispose(() => {
+        const oldShellNames = entryPoints.map(x => x.name)
+        console.debug(`----- HMR[${shortModuleId}] > REMOVING SHELLS >`, oldShellNames)
+        return window.repluggableAppDebug.host.removeShells(oldShellNames)
+    })
 
-        if (sourceModule.hot.addStatusHandler && sourceModule.hot.status() === 'idle') {
-            console.debug(`---- HMR[${shortModuleId}] > ADD STATUS HANDLER`)
-
-            sourceModule.hot.addStatusHandler(async (status: string) => {
-                console.debug(`-------- HMR[${shortModuleId}] > statusHandler(${status})`)
-                if (status === 'apply') {
-                    setTimeout(async () => {
-                        const entry = getOrAddModuleEntry(sourceModule.id, entryPoints)
-                        if (entry.newEntryPoints) {
-                            console.debug(`----- HMR[${shortModuleId}] > REMOVING SHELLS >`, entry.oldShellNames)
-
-                            await window.repluggableAppDebug.host.removeShells(entry.oldShellNames)
-
-                            console.debug(
-                                `----- HMR[${shortModuleId}] > ADDING SHELLS >`,
-                                entry.newEntryPoints.map(ep => ep.name)
-                            )
-
-                            window.repluggableAppDebug.host.addShells(entry.newEntryPoints)
-                            entry.oldShellNames = entry.newEntryPoints.map(ep => ep.name)
-                            entry.newEntryPoints = undefined
-                        }
-                    })
-                }
-            })
-        }
+    if (sourceModule.hot.status() === 'apply') {
+        console.debug(
+            `----- HMR[${shortModuleId}] > ADDING SHELLS >`,
+            entryPoints.map(x => x.name)
+        )
+        window.repluggableAppDebug.host.addShells(entryPoints)
     }
 
     return entryPoints
