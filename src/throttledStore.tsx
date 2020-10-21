86baa0068e48debc5cd5386acc40446098562e4f
Amir Yonatan <amiry@wix.com>
2020-10-21 17:51:56 +0300

    fixed package usage

--- src/throttledStore.tsx
+++ src/throttledStore.tsx
@@ -1,5 +1,5 @@
 import { Reducer, Action, createStore, Store } from 'redux'
-import { devToolsEnhancer } from 'redux-devtools-extension/developmentOnly'
+import { devToolsEnhancer } from 'redux-devtools-extension'
 import { AppHostServicesProvider } from './appHostServices'
 import _ from 'lodash'
 
