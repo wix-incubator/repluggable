e72cceca12fc5a5f314946ba5309b4d6484cc3f5
oleg <oleg@gordeev.cz>
2021-12-08 21:12:25 +0200

    add tests and change equal compare to compare element props

--- src/propsDeepEqual.tsx
+++ src/propsDeepEqual.tsx
@@ -1,9 +1,16 @@
 import _ from 'lodash'
+import { isValidElement } from 'react'
 
 export const propsDeepEqual = (propsA: any, propsB: any) => {
     const customizer: _.IsEqualCustomizer = (a, b, key, objectA) => {
-        if (key === '_owner') {
-            return true
+        if (isValidElement(a)) {
+            if (!isValidElement(b)) {
+                return false
+            }
+            if (a === b) {
+                return true
+            }
+            return a.key === b.key && a.type === b.type && propsDeepEqual(a.props, b.props)
         }
         if (key === 'children' && objectA === propsA) {
             if (typeof a === 'function' && typeof b === 'function') {
