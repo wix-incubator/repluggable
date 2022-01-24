049512fe3fe3846a4313653a94ce80781479f1b5
Ori Marron <orim@wix.com>
2022-01-24 12:55:45 +0200

    receive host + docs

--- src/hot.ts
+++ src/hot.ts
@@ -1,27 +1,44 @@
-import { EntryPoint } from './API'
-import { RepluggableAppDebugInfo } from './repluggableAppDebug/debug'
+import { AppHost, EntryPoint } from './API'
 
-export type Hot = (sourceModule: NodeModule, entryPoints: EntryPoint[], repluggableAppDebugObject?: RepluggableAppDebugInfo) => EntryPoint[]
+export type Hot = typeof hot
 
-export const hot: Hot = (sourceModule, entryPoints, repluggableAppDebugObject) => {
+/**
+ * To enable HMR, use the `hot` util to wrap the export of your repluggable package
+ *
+ * @param {NodeModule} sourceModule
+ * @param {EntryPoint[]} entryPoints
+ * @param {AppHost} [host] Optional. The AppHost to be used for removing / adding entrypoints. If not provided, `window.repluggableAppDebug.host` will be used,
+ * @example ```ts
+ * export default hot(module, [FooEntryPoint, BarEntryPoint])
+ * ```
+ */
+export function hot(sourceModule: NodeModule, entryPoints: EntryPoint[], host?: AppHost) {
     if (!sourceModule.hot) {
         return entryPoints
     }
 
-    sourceModule.hot.accept()
+    const hostInstance = host ?? window.repluggableAppDebug?.host
+    if (!hostInstance) {
+        console.error(
+            `HMR error: cannot find host object.\n` +
+                `hot(...) was called without the optional host parameter, and the fallback window.repluggableAppDebug.host doesn't exist`
+        )
+        return entryPoints
+    }
 
-    const debug = repluggableAppDebugObject ?? window.repluggableAppDebug
     const shellNames = entryPoints.map(x => x.name)
     const shortModuleId = sourceModule.id.split('/').pop()
 
+    sourceModule.hot.accept()
+
     sourceModule.hot.dispose(() => {
         console.debug(`----- HMR[${shortModuleId}] > REMOVING SHELLS >`, shellNames)
-        return debug.host.removeShells(shellNames)
+        return hostInstance.removeShells(shellNames)
     })
 
     if (sourceModule.hot.status() === 'apply') {
         console.debug(`----- HMR[${shortModuleId}] > ADDING SHELLS >`, shellNames)
-        debug.host.addShells(entryPoints)
+        hostInstance.addShells(entryPoints)
     }
 
     return entryPoints
