95b06161c3b78d3c524187451cd25ce9d9c2c286
Shirly Niego <64351631+shirlynwix@users.noreply.github.com>
2022-07-20 10:16:23 +0300

    Revert "Revert "fix for browser freeze issue (#176)" (#189)" (#195)
    This reverts commit 04b5530487da4a2df7cf37ec2d0008c683fb7e9c.

--- src/propsDeepEqual.tsx
+++ src/propsDeepEqual.tsx
@@ -1,7 +1,17 @@
 import _ from 'lodash'
+import { isValidElement } from 'react'
 
 export const propsDeepEqual = (propsA: any, propsB: any) => {
     const customizer: _.IsEqualCustomizer = (a, b, key, objectA) => {
+        if (isValidElement(a)) {
+            if (!isValidElement(b)) {
+                return false
+            }
+            if (a === b) {
+                return true
+            }
+            return a.key === b.key && a.type === b.type && propsDeepEqual(a.props, b.props)
+        }
         if (key === 'children' && objectA === propsA) {
             if (typeof a === 'function' && typeof b === 'function') {
                 return false
