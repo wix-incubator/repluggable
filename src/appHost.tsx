e4af359b1482004a241da9f06e65b508065947a7
Noa Cohen <91830681+noacoWix@users.noreply.github.com>
2023-05-28 16:23:58 +0300

    preserve memoized function name (#230)
    * preserve memoized function name
    
    * lint fix
    
    * lint fix
    
    * define function name only when debugMemoization is enabled
    
    * lint fix

--- src/appHost.tsx
+++ src/appHost.tsx
@@ -208,6 +208,10 @@ export function createAppHost(initialEntryPointsOrPackages: EntryPointOrPackage[
         }
         const memoized = _.memoize(func, resolver)
 
+        if (options.monitoring.debugMemoization) {
+            Object.defineProperty(memoized, 'name', { value: `${func.name}_memoized`, writable: false })
+        }
+
         if (options.monitoring.disableMonitoring) {
             return memoized
         }
