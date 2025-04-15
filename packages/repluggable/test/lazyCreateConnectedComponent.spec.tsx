15bf20a92617982975b98295dcea7e10ed0d50e6
Bohdan Lyzanets <bohdanl@wix.com>
2025-04-15 18:17:02 +0300

    improve tests

--- packages/repluggable/test/lazyCreateConnectedComponent.spec.tsx
+++ packages/repluggable/test/lazyCreateConnectedComponent.spec.tsx
@@ -4,69 +4,94 @@ import { lazyCreateConnectedComponent } from '../src'
 import { Shell } from '../src/API'
 import { addMockShell, collectAllTexts, connectWithShell, createAppHost, renderInHost } from '../testKit'
 
-function createFallbackTrackerComponent() {
-    let onMounted: () => void
-    let onUnmounted: () => void
+function createFallbackTrackerComponent(label: string = 'loading...') {
+    let handleMounted: () => void
+    let handleUnmounted: () => void
     const mountedPromise = new Promise<void>(resolve => {
-        onMounted = resolve
+        handleMounted = resolve
     })
     const unmountedPromise = new Promise<void>(resolve => {
-        onUnmounted = resolve
+        handleUnmounted = resolve
     })
 
     const Fallback: React.FC = () => {
         React.useEffect(() => {
-            onMounted()
-            return onUnmounted
+            handleMounted()
+            return handleUnmounted
         }, [])
-        return <div>Loading...</div>
+        return <div>{label}</div>
     }
 
     // NOTE: `Object.assign` is OK here,
     // so disable "prefer-object-spread"
     /* tslint:disable-next-line */
-    return Object.assign(Fallback, {
-        mounted: mountedPromise,
-        unmounted: unmountedPromise
+  return Object.assign(Fallback, {
+        LABEL: label,
+        waitMounted: () => mountedPromise,
+        waitUnmounted: () => unmountedPromise
     })
 }
 
-describe('lazyCreateConnectedComponent', () => {
-    const TEXT_IN_LAZY_COMPONENT = 'HELLO FROM LAZY COMPONENT'
+const TEXT_IN_LAZY_COMPONENT = 'HELLO FROM LAZY COMPONENT'
 
-    const lazyComponentFactoryMock = (() => {
-        interface MyComponentStateProps {
-            foo: string
-        }
+const lazyComponentFactoryMock = (() => {
+    interface MyComponentStateProps {
+        foo: string
+    }
 
-        const MyComponent: React.FC<MyComponentStateProps> = props => <div className="my-component">{props.foo}</div>
+    interface MyComponentOwnProps {
+        bar?: string
+    }
 
-        const createMyComponent = (boundShell: Shell) =>
-            connectWithShell<{}, {}, MyComponentStateProps>(
-                () => ({
-                    foo: TEXT_IN_LAZY_COMPONENT
-                }),
-                undefined,
-                boundShell
-            )(MyComponent)
+    const MyComponent: React.FC<MyComponentStateProps & MyComponentOwnProps> = props => (
+        <div className="my-component">
+            {props.foo}
+            <span>{props.bar}</span>
+        </div>
+    )
 
-        return {
-            loadWithNamedExport: async () => ({
-                createMyComponent
+    const createMyComponent = (boundShell: Shell) =>
+        connectWithShell<{}, MyComponentOwnProps, MyComponentStateProps>(
+            () => ({
+                foo: TEXT_IN_LAZY_COMPONENT
             }),
-            loadWithDefaultExport: async () => ({
-                default: createMyComponent
-            })
-        }
-    })()
+            undefined,
+            boundShell
+        )(MyComponent)
 
-    it('should create and render connected component from lazy factory (with default export)', async () => {
+    return {
+        /**
+         * represents a module with a named export
+         * @example
+         * ```
+         * export function createMyComponent(shell: Shell) {}
+         * ```
+         */
+        loadWithNamedExport: async () => ({
+            createMyComponent
+        }),
+        /**
+         * represents a module with a default export
+         * @example
+         * ```
+         * export default function createMyComponent(shell: Shell) {}
+         * ```
+         */
+        loadWithDefaultExport: async () => ({
+            default: createMyComponent
+        })
+    }
+})()
+
+describe.each([
+    ["with 'default export' module", () => lazyComponentFactoryMock.loadWithDefaultExport()],
+    ["with 'named export' module", () => lazyComponentFactoryMock.loadWithNamedExport().then(module => module.createMyComponent)]
+])('lazyCreateConnectedComponent (loadComponentFactory %s)', (_, loadComponentFactory) => {
+    it('should create and render connected component from lazy factory', async () => {
         const host = createAppHost([])
         const boundShell = addMockShell(host)
 
-        const loadComponentFactory = lazyComponentFactoryMock.loadWithDefaultExport
-
-        const MyComponent = lazyCreateConnectedComponent(boundShell, () => loadComponentFactory())
+        const MyComponent = lazyCreateConnectedComponent(boundShell, loadComponentFactory)
 
         const FallbackTracker = createFallbackTrackerComponent()
 
@@ -77,48 +102,48 @@ describe('lazyCreateConnectedComponent', () => {
             host
         )
 
-        // Check that the component is not rendered yet
-        // and the fallback is shown
+        // Check that the component shows the fallback, while the lazy component is loading
+        expect(collectAllTexts(parentWrapper)).toContain(FallbackTracker.LABEL)
         expect(collectAllTexts(parentWrapper)).not.toContain(TEXT_IN_LAZY_COMPONENT)
 
-        await FallbackTracker.mounted
-        await FallbackTracker.unmounted
+        await FallbackTracker.waitUnmounted()
 
+        // Check that the component shows the loaded content
+        expect(collectAllTexts(parentWrapper)).not.toContain(FallbackTracker.LABEL)
         expect(collectAllTexts(parentWrapper)).toContain(TEXT_IN_LAZY_COMPONENT)
     })
 
-    it('should create and render connected component from lazy factory (with named export)', async () => {
+    it('should create and render connected componet with forwarding own props', async () => {
         const host = createAppHost([])
         const boundShell = addMockShell(host)
 
-        const loadComponentFactory = lazyComponentFactoryMock.loadWithNamedExport
-
-        const MyComponent = lazyCreateConnectedComponent(boundShell, () => loadComponentFactory().then(module => module.createMyComponent))
+        const MyComponent = lazyCreateConnectedComponent(boundShell, loadComponentFactory)
 
         const FallbackTracker = createFallbackTrackerComponent()
 
+        const TEST_PROP = 'HELLO_FROM_PROPS'
+
         const { parentWrapper } = renderInHost(
             <React.Suspense fallback={<FallbackTracker />}>
-                <MyComponent />
+                <MyComponent bar={TEST_PROP} />
             </React.Suspense>,
             host
         )
 
-        expect(collectAllTexts(parentWrapper)).not.toContain(TEXT_IN_LAZY_COMPONENT)
+        expect(collectAllTexts(parentWrapper)).not.toContain(TEST_PROP)
 
-        await FallbackTracker.mounted
-        await FallbackTracker.unmounted
+        await FallbackTracker.waitUnmounted()
 
-        expect(collectAllTexts(parentWrapper)).toContain(TEXT_IN_LAZY_COMPONENT)
+        expect(collectAllTexts(parentWrapper)).toContain(TEST_PROP)
     })
 
     it('should call `loadComponentFactory` only after first render', async () => {
         const host = createAppHost([])
         const boundShell = addMockShell(host)
 
-        const loadComponentFactorySpy = jest.fn(lazyComponentFactoryMock.loadWithDefaultExport)
+        const loadComponentFactorySpy = jest.fn(() => loadComponentFactory())
 
-        const MyComponent = lazyCreateConnectedComponent(boundShell, () => loadComponentFactorySpy())
+        const MyComponent = lazyCreateConnectedComponent(boundShell, loadComponentFactorySpy)
 
         expect(loadComponentFactorySpy).not.toHaveBeenCalled()
 
@@ -136,9 +161,9 @@ describe('lazyCreateConnectedComponent', () => {
         const host = createAppHost([])
         const boundShell = addMockShell(host)
 
-        const loadComponentFactorySpy = jest.fn(lazyComponentFactoryMock.loadWithDefaultExport)
+        const loadComponentFactorySpy = jest.fn(() => loadComponentFactory())
 
-        const MyComponent = lazyCreateConnectedComponent(boundShell, () => loadComponentFactorySpy())
+        const MyComponent = lazyCreateConnectedComponent(boundShell, loadComponentFactorySpy)
 
         expect(loadComponentFactorySpy).not.toHaveBeenCalled()
 
